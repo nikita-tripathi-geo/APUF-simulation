ee3bf17df53a156d8ce2ca0e3a72b8ce26ed112f
Nikita Tripathi <ntripathi99@gmail.com>
2025-05-09 00:25:20 -0600

    updated __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,25 +1,21 @@
 # src/apuf/__init__.py
 
 """
-apuf - a library for simulating Arbiter PUFs and estimating response entropy.
+apuf - a library for simulating Arbiter PUFs and other delay-based PUFs.
 """
 
 # Package version (keep in sync with pyproject.toml or bump manually)
 __version__ = "0.1.0"
 
 # Core classes / functions exposed at the top level
-from .apuf import APUF
-from .challenges import generate_k_challenges, generate_n_k_challenges, generate_challenges_mp
-# from .multiproc import generate_challenges_mp
-# from .utils import parity
+from .apuf import APUF, XORPUF
+from .challenges import generate_k_challenges, generate_n_k_challenges
 
-# Define what’s exported when someone does:
-#   from apuf import *
+# Define what’s exported when someone does: from apuf import *
 __all__ = [
     "APUF",
+    "XORPUF",
     "generate_k_challenges",
     "generate_n_k_challenges",
-    # "generate_challenges_mp",
-    # "parity",
     "__version__",
 ]
